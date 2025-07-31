20756e3468993aa6626ee1eccf9f531df9ba5a70
Vaishnavi Bhangare <155755501+Abcvai@users.noreply.github.com>
2025-07-31 22:50:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let a=91;
+let b=500;
+let c=100;
+let d=20
+
+if(a>b && a>c || a>d){
+    console.log(" A is greater");
+}
+else if(b>a && b>c){
+    console.log("B is greater");
+}
+else{
+    console.log("C is greater")
+}
