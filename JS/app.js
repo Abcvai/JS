fcabfa41848bb217577b6fdd87181da50e1f707f
vaishnavi bhangare <vaishnavibhangare613@gmail.com>
2025-08-07 14:11:19 +0530

    practice questions.

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,68 @@
-alert("something is wrong!");
+// let num = 25;
+
+// if(num%10==0)
+// {
+//     console.log("good");
+// }
+//     else{
+//         console.log("bad");
+//     }
+
+
+// let uname = prompt("Enter user Name = ");
+// let age = prompt("Enter age = ");
+
+// alert(`${uname} is ${age} years old`);
+
+//  let quarter=1;
+
+// switch(quarter){
+//     case 1 : console.log("January,February,March,");
+//     break;
+    
+//     case 2 : console.log("April,May,June");
+//     break;
+
+//     case 3: console.log("July,August,September");
+//     break;
+
+//     case 4 : console.log("October November,December");
+//     break;
+
+//     default: console.log("not a quarter")
+
+
+// }
+
+let string = "apples";
+
+if((string[0]=='a'|| start[0]=='A') &&  (string.length > 5)){
+    console.log("golden string")
+}
+
+else{
+    console.log("not a golden string.")
+}
+
+
+let a=10;
+let b=200;
+let c=100;
+ if(a>b && a>c){
+    console.log(`${a} is greater`);
+ }
+ if(b>a && b>c){
+    console.log(`${b} is greater`);
+ }
+ else{
+    console.log(`${c} is greater`);
+ }
+
+ let num1 = 32;
+ let num2 = 478531;
+ if((num1%10) == (num2%10)){
+    console.log("numbers have the same last digit which is",num1%10);
+ }
+ else{
+    console.log("number don't have the same last deigit");
+ }
